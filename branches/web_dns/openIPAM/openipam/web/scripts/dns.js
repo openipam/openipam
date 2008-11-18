175bcc81bee4aa27443d66eb42e412ac03ac9ed9
italianwaite <italianwaite@506c45b4-1d4b-0410-b623-73d68388d251>
2008-11-18 18:10:31 +0000

    -Modified template and javascript for dns submission stuff

--- branches/web_dns/openIPAM/openipam/web/scripts/dns.js
+++ branches/web_dns/openIPAM/openipam/web/scripts/dns.js
@@ -20,21 +20,72 @@ function deleteChecked(i, k) {
 };
 
 function saveRecords() {
-	//bind to a key
-	$('#saveRecords').click();
 	//create list
 	submitList = [];
-	//loop through rows
-	temp = $('.info').find(name);
-	console.log(temp);
-		//create dictionary inside row
-		//add data to dictionary using the id as the key and the content as the value
-		//add deleted flag
+	
+	//loop through existing rows
+	rows = $('tr.info').each(function(){
+		temp_record = {};
+		
+		temp_record['id'] = $(this).attr('id');
+
+		if($(this).find('input[name="delete"]:checked').val()){
+			temp_record['deleted'] = true;
+		}
+		else{
+			var name = $(this).find('input[name="name"]').val();
+			temp_record['name'] = name;
+			temp_record['tid'] = $(this).find('select[name="tid"]').val();
+			var content = $(this).find('input[name="content"]').val();
+			temp_record['content'] = content;
+		}
+		
+		if(temp_record['deleted'] || (name || content)){
+			submitList.push(temp_record);
+		}
+		
+	}); //end rows loop
+	
+	//loop through new rows
+	newRows = $('tr.infoNew').each(function(){
+		new_record = {};
+		
+		if($(this).find('input[name="no_add"]:checked').val()){
+			new_record['no_add'] = true;
+		}
+		else{
+			var name = $(this).find('input[name="name"]').val();
+			new_record['name'] = name;
+			new_record['tid'] = $(this).find('select[name="tid"]').val();
+			var content = $(this).find('input[name="content"]').val();
+			new_record['content'] = content;
+		}
+		
+		if((name || content) && !new_record['no_add']){
+			submitList.push(new_record);
+		}
+		
+	}); // end newRows loop
+	
 	//sent list to webservice
+	console.log(submitList);
+	
+	$.ajax({
+		url: "/ajax/ajax_change_dns_records",
+		data: submitList,
+		success: function(){
+			alert("Success");
+		}
+	});
+	
+	return false;
 };
 
 
 $(function() {
+	//bind to a key
+	$('#saveRecords').click( saveRecords );
+	
 	var counter = 0;	
 	$('tr.infoNew').hide();
 	$('#addRecord').click(function(){
@@ -55,10 +106,6 @@ $(function() {
 	if($('#quickAdd').length){
 		$('#addRecord').click();
 	};
-	
-	var temp = $('tr.info').find(name);
-	console.log('hello');
-	console.log(temp);
 	
 	names = [];
 	
