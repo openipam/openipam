af82abcdd7dc30f67548d390e49da99358d87a4d
ekoyle <ekoyle@506c45b4-1d4b-0410-b623-73d68388d251>
2009-06-30 23:12:01 +0000

    Delete the ipv6 branch for now.

--- branches/ipv6/openIPAM/openipam_config.example/dhcp.py
+++
@@ -1,7 +0,0 @@
-# The default DHCP lease time for all statically assigned addresses. Dynamic
-# lease times are configured on the pool.
-static_lease_time = 86400
-
-# The IP address of this server
-server_ip = '192.168.0.1'
-
