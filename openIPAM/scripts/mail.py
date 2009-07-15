340e6285afb25ea2c88e80c1211648a9c99485be
ekoyle <ekoyle@506c45b4-1d4b-0410-b623-73d68388d251>
2009-07-15 22:13:18 +0000

    An "example" notification script...

--- openIPAM/scripts/mail.py
+++ openIPAM/scripts/mail.py
@@ -1,5 +1,13 @@
 import email.Message
 import smtplib
+import time
+import random
+from socket import getfqdn
+import math
+
+random.seed()
+
+hostname = getfqdn()
 
 def mail(serverURL=None, sender='', to='', subject='', text=''):
 	message = email.Message.Message()
@@ -10,3 +18,59 @@ def mail(serverURL=None, sender='', to='', subject='', text=''):
 	mailServer = smtplib.SMTP(serverURL)
 	mailServer.sendmail(sender, to, message.as_string())
 	mailServer.quit()
+
+base64map = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+/'
+def base64enc( data ):
+	cur = data
+	exponent = int(math.log(data,64))
+	vals = []
+	while exponent >= 0:
+		c = 64**exponent
+		cm = cur/c
+		cur = cur%c
+		vals.append(base64map[cm])
+		exponent-=1
+	return ''.join(vals)
+
+def generate_message_id(message):
+	parts = []
+	to = 'NULL'
+	if message.has_key('To'):
+		to = message['To']
+	t = int(time.time()*1000000)
+	t = base64enc(t)
+	str = '<%s-%s-%s@%s>' % (t,base64enc(random.getrandbits(32)),to.replace('@','_'),hostname)
+	return str
+
+class Mailer(object):
+	def __init__(self, server):
+		self.server = server
+		# can we increase efficiency somehow?
+		#self.relay = smtplib.SMTP(server)
+
+	def send_msg( self, to=None, sender=None, subject=None, body=None, headers=None):
+		if not headers:
+			headers={}
+		message = email.Message.Message()
+		message["To"]      = to
+		message["From"]    = sender
+		message["Subject"] = subject
+		for k in headers:
+			message[k] = headers[k]
+		message.set_payload(body)
+
+		self.single_msg(sender=sender, to=to, message=message)
+
+	def single_msg( self, sender, to, message):
+		if not message.has_key('Date'):
+			message['Date'] = time.strftime('%a, %d %b %H:%M:%S %z')
+		if not message.has_key('Message-ID'):
+			message['Message-ID'] = generate_message_id(message)
+		relay = smtplib.SMTP(self.server)
+		relay.sendmail(sender, to, message.as_string())
+		relay.quit()
+
+	#def __del__(self):
+	#	self.relay.quit()
+
+
--- openIPAM/scripts/notification.py
+++ openIPAM/scripts/notification.py
@@ -1,10 +1,17 @@
 import sys
 from openipam.backend.db import interface
 from openipam.backend.auth.interfaces import LDAPInterface
-from mail import *
+#from mail import *
+import scripts.mail
+
+from openipam.config import auth_sources
 
 db = interface.DBInterface(username='admin')
 ldap_interface = LDAPInterface()
+
+fromaddr = 'servicedesk@usu.edu'
+replyaddr = 'servicedesk@usu.edu'
+bounceaddr = 'bounce@usu.edu'
 
 # Get list of people who need to be notified.
 notification_list = db.find_expiring_hosts()
@@ -14,36 +21,45 @@ notification_list = db.find_expiring_hosts()
 
 contactlist={}
 
-dynamic_msg = """Subject: Your USU computer registration%(plural)s %(is_are)s about to expire
-
-%(name)s,
+dynamic_subject = '[USU:Important] Your USU computer registration%(plural)s %(is_are)s about to expire'
+dynamic_msg = """%(name)s (%(username)s),
 
 The following computer%(plural)s %(is_are)s going to expire soon.
+
 If you would like to continue using the USU network for another year:
 
-1. Login to https://bluezone.usu.edu
-2. Click "Renew" next to the following computer%(plural)s.
+1. Log in to https://bluezone.usu.edu
+2. Click "Renew" next to the following computer%(plural)s:
 
 %(rows)s
 
 If you have any questions, please contact the IT Service Desk.
+
 Remember: never give your password to anyone, including USU employees.
 
+-- 
 IT Service Desk
-Phone: 435.797.HELP (4357)
-Toll Free: 1.877.878.8325
+Summer Hours:
+Mon-Thu: 8:00am - 8:00pm
+Friday: 8:00am - 6:00pm
+
+Contact us at: 
+Phone: 797-HELP (4357)
+Toll Free: 877-878-8325
 Email: servicedesk@usu.edu
+http://footprints.usu.edu (Issue Tracking System)
+
 
 """
 
-static_msg = """Subject: openIPAM Host Renewal Notice
-
-%(name)s,
+static_subject = '[USU:Important] openIPAM Host Renewal Notice'
+static_msg = """%(name)s (%(username)s),
 
 The following computer%(plural)s %(is_are)s going to expire soon.
+
 To renew your servers and clients for another year:
 
-1. Login to https://openipam.usu.edu
+1. Log in to https://openipam.usu.edu
 2. Click "Show my hosts expiring within 60 days"
 3. Check the boxes next to those hosts you wish to renew
 4. At the bottom, choose "Renew selected hosts" and click "Go"
@@ -54,10 +70,18 @@ Remember: help us keep up-to-date data. Don't renew hosts you don't need.
 
 If you have any questions, please contact the IT Service Desk.
 
+-- 
 IT Service Desk
-Phone: 435.797.HELP (4357)
-Toll Free: 1.877.878.8325
+Summer Hours:
+Mon-Thu: 8:00am - 8:00pm
+Friday: 8:00am - 6:00pm
+
+Contact us at: 
+Phone: 797-HELP (4357)
+Toll Free: 877-878-8325
 Email: servicedesk@usu.edu
+http://footprints.usu.edu (Issue Tracking System)
+
 
 """
 
@@ -69,35 +93,53 @@ row_fmt = "%(hostname)-40s %(mac)-22s %(days)3s days      %(description)s"
 # Create a dictionary with keys of an A Number and values of the hosts about to expire.
 for rowitem in notification_list:
 	row_text = row_fmt % rowitem
-	if contactlist.has_key('%(username)s' % rowitem):
+	if contactlist.has_key(rowitem['username']):
 		if not contactlist[rowitem['username']]['is_static'] and rowitem['is_static']:
 			contactlist[rowitem['username']]['is_static'] = True
-		contactlist['%(username)s' % rowitem]['rows'].append(row_text)
+		contactlist[rowitem['username']]['rows'].append(row_text)
+		contactlist[rowitem['username']]['notifications'].append(rowitem['nid'])
 	else:
-		contactlist['%(username)s' % rowitem] = {'rows':[row_heading,row_text,],'is_static':rowitem['is_static'],}
+		contactlist['%(username)s' % rowitem] = {'rows':[row_heading,row_text,],'is_static':rowitem['is_static'],'notifications':[rowitem['nid'],]}
 	# TODO: remove the items from the notification_to_hosts table.
+
+
+#mailer = scripts.mail.Mailer('mail.usu.edu')
+mailer = scripts.mail.Mailer('127.0.0.1')
 
 for item in contactlist:
 	try:
-		ldapquery = ldap_interface._search_ldap(username=item)
+		user = auth_sources.get_info(username=item)
+		if not user:
+			raise Exception('User %s not found' % item)
+		if not user.email:
+			raise Exception('User %s does not have preferred email set' % item)
 	except Exception, e:
-		print e
+		sys.stderr.write( '%s: %s\n%s\n\n' % (e,item,'\n'.join(contactlist[item]['rows'])) )
+		continue
 	#print item
 	#print contactlist[item]
-	contactlist[item]['name'] = ldapquery['name']
-	contactlist[item]['email'] = ldapquery['email']
-	contactlist[item]['email'] = ldapquery['email']
+	contactlist[item]['name'] = user.name #ldapquery['name']
+	contactlist[item]['email'] = user.email #ldapquery['email']
+	contactlist[item]['username'] = user.username #item
 	num_rows = len(contactlist[item]['rows']) - 1
 	contactlist[item]['rows'] = '\n'.join(contactlist[item]['rows'])
-	contactlist[item]['plural'] = 's' if num_rows > 2 else ''
-	contactlist[item]['is_are'] = 'are' if num_rows > 2 else 'is'
+	contactlist[item]['plural'] = 's' if num_rows > 1 else ''
+	contactlist[item]['is_are'] = 'are' if num_rows > 1 else 'is'
 	if contactlist[item]['is_static']:
 		emailtext = static_msg
+		subject = static_subject % contactlist[item]
 	else:
 		emailtext = dynamic_msg
+		subject = dynamic_subject % contactlist[item]
 	emailtext = emailtext % contactlist[item]
+	print '----------------------------------------------'
+	print """To: %s\nFrom: %s\nReply-to: %s\nSubject: %s\n""" % (contactlist[item]['email'],fromaddr,replyaddr,subject)
 	print emailtext
 	print '----------------------------------------------'
+	print 'Delete: %s' % contactlist[item]['notifications']
+
+	mailer.send_msg(to=contactlist[item]['email'], sender=fromaddr, subject=subject, body=emailtext, headers={'Reply-to':replyaddr,'Return-Path':bounceaddr})
+	db.del_notification_to_host(id=contactlist[item]['notifications'])
 
 	# TODO: Fill in to field key value.
 	#mail("mail.example.com", "noreply@example.com", "%()s" % ldapquery, "Your registered computer will expire soon", emailtext);
