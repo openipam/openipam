bf59ae9a6ad4bcc66d92d3aabb8a2421af9dfd21
ekoyle <ekoyle@506c45b4-1d4b-0410-b623-73d68388d251>
2009-08-13 21:42:07 +0000

    Mmm... arp goodness...; also, make a 'delete' on any table with 'changed'/'changed_by' do an update first to keep our audit trail

---
+++ openIPAM/openipam/extensions/arp.py
@@ -0,0 +1,12 @@
+# This should look something like:
+
+#min_permissions = '00000100'
+#
+#def byip(address):
+#	pass
+
+#def bymac(address):
+#	pass
+
+from openipam_config.extensions.arp import *
+
