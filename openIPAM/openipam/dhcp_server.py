25ea82ca6ccb5c9170ff258b1142ee0f9bca98d0
ekoyle <ekoyle@506c45b4-1d4b-0410-b623-73d68388d251>
2015-01-08 18:27:36 +0000

    Don't try to make assignment in if condition

--- openIPAM/openipam/dhcp_server.py
+++ openIPAM/openipam/dhcp_server.py
@@ -335,7 +335,7 @@ def log_packet( packet, prefix='', level=dhcp.logging.INFO):
 		client_foo = str(client)
 
 	dhcp.get_logger().log(level, "%-12s %-8s %s 0x%08x (%s)", prefix, t_name, mac, xid, client_foo )
-	if raven_client and level >= raven_client_min_level or t_name='decline':
+	if raven_client and level >= raven_client_min_level or t_name=='decline':
 		if 'IGN' in prefix:
 			if 'LIMIT' in prefix:
 				message = 'request from %s ignored due to rate limiting' % mac
